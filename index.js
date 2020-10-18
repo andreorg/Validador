19426a75dd543bc6b4b55a838058294bb5bd1407
andreorg <71940629+andreorg@users.noreply.github.com>
2020-10-17 22:16:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function validation(expresion){
+const RegExp = /^[A-Z]\d{3}\w{3}[!"#%&=¿?]{3}/
+    if(RegExp.test(expresion)){
+        document.getElementById("rest").innerHTML = "CONTRASEÑA VALIDA"
+        document.getElementById("rest2").innerHTML = "Contraseña aceptada satisfactoriamente"
+    }
+    else{
+        document.getElementById("rest").innerHTML = "CONTRASEÑA INVALIDA"
+        document.getElementById("rest2").innerHTML = "Contraseña incorrecta, intentelo otra vez"
+    }
+        
+}
